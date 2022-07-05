3a2678ee35d7cf616e6ff007cbf4deea3554947e
Roy Ratcliffe <roy@ratcliffe.me>
2022-07-05 20:32:11 +0100

    fix: try using href

--- R/hi.R
+++ R/hi.R
@@ -1,7 +1,7 @@
 #' Redis High-Level Interface
 #' @inheritDotParams redux::hiredis
 #' @return Redis interface facade, R6 \code{redis_api} instance
-#' @seealso \link[https://github.com/redis/hiredis]{Minimalistic C client
+#' @seealso \href{https://github.com/redis/hiredis}{Minimalistic C client
 #'   library for the Redis database}
 #' @export
 #' @examples
